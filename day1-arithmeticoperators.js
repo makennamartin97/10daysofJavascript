a25319a1016d81acd482c5d6ccb0d30338bc233f
makennamartin97 <makennamartin97@yahoo.com>
2020-09-08 18:16:35 -0400

    day 1: arithmetic operators

---
+++ day1-arithmeticoperators.js
@@ -0,0 +1,30 @@
+
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area = length * width;
+
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter = length*2 + width*2;
+    // Write your code here
+    
+    return perimeter;
+}
